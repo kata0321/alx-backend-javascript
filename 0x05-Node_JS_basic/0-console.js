50dfbb95d6ec4f0bf423c9c2868bf2dddd87b3fb
root <tpgare3@gmail.com>
2024-09-18 17:55:04 +0000

    task0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Ettouffah ayoub <https://github.com/kata0321>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
